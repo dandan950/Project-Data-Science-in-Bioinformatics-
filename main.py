c5164d479c22c5a8da0f441cd614519164616af5
Ruidan LIU <57582693+dandan950@users.noreply.github.com>
2023-02-06 22:43:19 +0100

    updata , add pipeline2

--- main.py
+++ main.py
@@ -9,9 +9,13 @@ def pangolinexec(inputfile="", outputfile=""):
         if outputfile!="":
             execstr=execstr + " --outfile " + outputfile
     return execstr+"; "
+
 SAMPLES = ["RV417026_S15_L001", "RV417027_S18_L001", "RV417028_S20_L001","RV417029_S19_L001"]
+
+#display results
 def final_display():
     #print(" Pangolin don't have B.1.545 from it's lineage list: https://cov-lineages.org/lineage_list.html ")
+    print("The following is leneage detection results of using reference-guided assembly pipeline:")
     for i in SAMPLES:
         filename="pipeline_ref_result/"+i+'.csv'
 
@@ -19,14 +23,23 @@ def final_display():
             reader = csv.DictReader(csvfile)
             for row in reader:
                 print("From ",i,"the lineage is:",row['lineage'])
+    print("The following is leneage detection results of using de novo assembly pipeline:")
+    for i in SAMPLES:
+        filename="pipeline_deNovo_ragtag_output/scaffold/"+i+"/"+i+'.csv'
 
+        with open(filename, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                print("From ",i,"the lineage is:",row['lineage'])
+
+#generate midresult
 def bwa_index():
     pass
     makefile_str="mkdir midresult;cd midresult;touch ref.amb ref.ann ref.bwt ref.pac ref.sa; cd ..;"
     bwa_str="bwa index -p midresult/ref workflow/ref.fasta;"
     return makefile_str+bwa_str
 
-
+#run snakemake of first pipeline(use reference-guide assembly)
 def run_snakemake():
     envstr="source ~/mambaforge/etc/profile.d/conda.sh;conda activate pangolin;"
     snakemakestr=" snakemake -c10  --use-conda;"
@@ -38,6 +51,19 @@ def run_snakemake():
     print(condastr)
     os.system(condastr)
 
+#run snakemake of second pipeline(use de novo assembly)
+def run_snakemake2():
+    envstr="source ~/mambaforge/etc/profile.d/conda.sh;conda activate pangolin;"
+    snakemakestr=" snakemake -s workflow2/snakefile -c10  --use-conda --jobs 2;"
+    bwa=bwa_index()
+    pangolinstr=""
+    for i in SAMPLES:
+        pangolinstr+=pangolinexec("pipeline_deNovo_ragtag_output/scaffold/{0}/ragtag.scaffold.fasta".format(i),"pipeline_deNovo_ragtag_output/scaffold/{0}/{0}.csv".format(i))
+    condastr="bash -c \" {0} {1} {2} {3}\"".format(envstr,bwa,snakemakestr,pangolinstr)
+    print(condastr)
+    os.system(condastr)
+
 if __name__ == "__main__":
-    run_snakemake()
+    #run_snakemake()
+    #run_snakemake2()
     final_display()
