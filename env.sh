af9d30d307a7fa72cab31d79eefe598ee049282f
Ruidan LIU <57582693+dandan950@users.noreply.github.com>
2023-02-10 16:27:32 +0100

    Update env.sh

--- env.sh
+++ env.sh
@@ -3,13 +3,17 @@
 
 echo "befor the install, assume the computer have Mambaforege in the path: ~/mambaforge";
 source ~/mambaforge/etc/profile.d/conda.sh;
-source ~/miniconda3/etc/profile.d/conda.sh;
+# automatically install mamba
 conda install -n base -c conda-forge mamba -y;
 source ~/mambaforge/etc/profile.d/conda.sh;
+# automatically install pangolin from github and activate pangolin
 git clone https://github.com/cov-lineages/pangolin.git;
 cd pangolin; 
 conda env create -f environment.yml; 
 conda activate pangolin;
+# install pangolin
 pip install .;
+# automatically install snakemake
 mamba install -c conda-forge -c bioconda snakemake -y;
+# automatically install bwa
 mamba install -c conda-forge -c bioconda bwa -y;
