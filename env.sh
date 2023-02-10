44e45ed7f20514b38ea75ec5d33399efbe43d445
Ruidan LIU <57582693+dandan950@users.noreply.github.com>
2023-02-10 16:23:57 +0100

    update environment requirement

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-echo "befor the install, assume the computer have Miniconda";
+echo "befor the install, assume the computer have Mambaforege in the path: ~/mambaforge";
 source ~/mambaforge/etc/profile.d/conda.sh;
 source ~/miniconda3/etc/profile.d/conda.sh;
 conda install -n base -c conda-forge mamba -y;
